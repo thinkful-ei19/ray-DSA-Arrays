bd7d5913d15a53168d79d92548d6ec8736534652
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-16 09:44:37 -0700

    added answers to questions

--- array-class.js
+++ array-class.js
@@ -73,16 +73,40 @@ function main() {
   let rayArray = new Array();
   
   //add an item to the array
-  rayArray.push(3);
-  rayArray.push(5);
-  rayArray.push(15);
-  rayArray.push(19);
-  rayArray.push(45);
-  rayArray.push(10);
+//   rayArray.push(3);
+//   rayArray.push(5);
+//   rayArray.push(15);
+//   rayArray.push(19);
+//   rayArray.push(45);
+//   rayArray.push(10);
+//   rayArray.pop();
+//   rayArray.pop();
+//   rayArray.pop();
+  rayArray.push('tauhida');
+  console.log('first item in array is',rayArray.get(0));
   console.log(rayArray);
 }
 main();
 
-// Length is 1, Capacity is 3, Memory Address ptr is at 0
+// Length is 1, Capacity is 3, Memory Address ptr is 0
 
-// Length is 6, Capacity is 12, Memory Address ptr is at 3
+// Length is 6, Capacity is 12, Memory Address ptr is 3
+// Capcity is 12 because when we make our push, the if statement in the push method sees that this.length is bigger than this._capacity,
+// so it then adds one to the length and multiplies by the array.size.ratio, giving us 12
+
+
+// After .pop()
+// Length is 3, Capacity is 12, Memory Address ptr is 3
+// After the 3 .pop()'s, our array length is reduced down to 3, but the memory capacity remained unchanged at 12. That extra capacity 
+// is left unchanged to leave room for our array to grow into later. 
+
+
+// First item in the array is 3
+
+
+// After emptying the array and pushing "tauhida", when we try to print out the one item, the result is NaN. 
+
+
+// The purpose of the _resize() function is to allocate more memory space for actions that need to be stored in our memory. The resize function
+// gets called by the push() and insert() functions when an action requires memory space. Those actions will pass a size length needed to the
+// resize() function and the resize() function will allocate the necessary amount of memory space for those action functions
