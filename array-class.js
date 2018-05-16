0a914abab2a048932fe8bcadf18ddda44f7c45fe
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-16 09:18:02 -0700

    added push

--- array-class.js
+++ array-class.js
@@ -1,28 +1,29 @@
-const memory = require('.memory');
+const Mem = require('./memory');
+const Memory = new Mem();
 
-class rayArray {
+class Array {
   constructor() {
     this.length = 0;
     this._capacity = 0;
-    this.ptr = memory.allocate(this.length);
+    this.ptr = Memory.allocate(this.length);
   }
 
   push(value) {
     if(this.length >= this._capacity) {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
-    memory.set(this.ptr + this.length, value);
+    Memory.set(this.ptr + this.length, value);
     this.length++;
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
-    this.ptr = memory.allocate(size);
+    this.ptr = Memory.allocate(size);
     if(this.ptr === null) {
-      throw new Error('Out of memory');
+      throw new Error('Out of Memory');
     }
-    memory.copy(this.ptr, oldPtr, this.length);
-    memory.free(oldPtr);
+    Memory.copy(this.ptr, oldPtr, this.length);
+    Memory.free(oldPtr);
     this._capacity = size;
   }
 
@@ -30,14 +31,14 @@ class rayArray {
     if(index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    return memory.get(this.ptr + index);
+    return Memory.get(this.ptr + index);
   }
 
   pop() {
     if(this.length == 0) {
       throw new Error('Index error');
     }
-    const value = memory.get(this.ptr + this.length - 1);
+    const value = Memory.get(this.ptr + this.length - 1);
     this.length--;
     return value;
   }
@@ -49,8 +50,8 @@ class rayArray {
     if(this.length >= this._capacity) {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
-    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-    memory.set(this.ptr + index, value);
+    Memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    Memory.set(this.ptr + index, value);
     this.length++;
   }
 
@@ -58,8 +59,30 @@ class rayArray {
     if(index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    Memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
     this.length--;
   }
 }
 Array.SIZE_RATIO = 3;
+
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  //create an instance of the array class
+  let rayArray = new Array();
+  
+  //add an item to the array
+  rayArray.push(3);
+  rayArray.push(5);
+  rayArray.push(15);
+  rayArray.push(19);
+  rayArray.push(45);
+  rayArray.push(10);
+  console.log(rayArray);
+}
+main();
+
+// Length is 1, Capacity is 3, Memory Address ptr is at 0
+
+// Length is 6, Capacity is 12, Memory Address ptr is at 3
--- memory.js
+++ memory.js
@@ -1,4 +1,4 @@
-class memory {
+class Memory {
     constructor() {
       this.memory = new Float64Array(1024);
       this.head = 0;
@@ -44,4 +44,4 @@ class memory {
     }
 }
   
-module.exports = memory;
+module.exports = Memory;
